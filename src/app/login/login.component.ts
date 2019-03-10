e9e3b4edefefbae5b22fcfa8f4066b2d46f733e7
rokasA <rokasalech@gmail.com>
2019-03-10 19:37:23 +0200

    Colors converted to hex properly
    prettified
    
    Cities renamed to regions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,7 @@ import { AlertService } from '../services/alert.service';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.scss', '../app.component.scss']
 })
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
