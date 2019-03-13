4e63cf802f3d3135c17a4cb9c4fa0a0f9b5cbc77
JulitaV <julita.vainickaite@gmail.com>
2019-03-13 13:41:41 +0200

    main page responsive design

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
     constructor() {}
+
+    breakpoint = 3;
+    colspan = 2;
+    rowspanText = 1;
+    rowspanIcon = 1;
+
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 973) ? 1 : 3;
+    this.colspan = (window.innerWidth <= 973) ? 1 : 2;
+    this.rowspanText = (window.innerWidth <= 973) ? 2 : 1;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 973) ? 1 : 3;
+    this.colspan = (window.innerWidth <= 973) ? 1 : 2;
+    this.rowspanText = (window.innerWidth <= 973) ? 2 : 1;
+  }
 }
+
