d50899867bd87f5c2d4a01673a18915c9b260b9a
JulitaV <julita.vainickaite@gmail.com>
2019-03-20 11:26:24 +0200

    fixing story images

---
+++ src/app/components/image-dialog/image-dialog.component.ts
@@ -0,0 +1,22 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-image-dialog',
+  templateUrl: './image-dialog.component.html',
+  styleUrls: ['./image-dialog.component.css']
+})
+export class ImageDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  done(): void {
+    this.dialogRef.close();
+  }
+  ngOnInit() {
+  }
+
+}
